7a19fe03795bcd4ee1783bf0d170ff77b149e347
vikkev <vinnibaraki@gmail.com>
2025-04-14 20:32:43 -0300

    CREATE: Skeleton

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import {heroui} from "@heroui/theme"
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+      colors: {
+        'nav-green': '#31511E',
+        'bg-green' : '#1A1A19'
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
+
+module.exports = config;
